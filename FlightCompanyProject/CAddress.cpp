6925ae831efe0c281e4e42c9e0bd2359938f879f
Tomer Stoianov <Tomer.Stoianov@s.afeka.ac.il>
2025-08-17 17:29:32 +0300

    commit

--- FlightCompanyProject/CAddress.cpp
+++ FlightCompanyProject/CAddress.cpp
@@ -5,7 +5,9 @@
 using namespace std;
 
 CAddress::CAddress(string cityName = "Tel Aviv", string streetName, int houseNumber) {
-
+	this->cityName = cityName;
+	this->streetName = streetName;
+	this->houseNumber = houseNumber;
 }
 string CAddress::getCityName() {
 	return this->cityName;
@@ -20,6 +22,11 @@ void CAddress::print() {
 	cout << "City name:%s, " << cityName << "Street name:%s, " << streetName << "House number:%d." <<
 		houseNumber << endl;
 }
-void CAddress::updateAddress(string cityName, string streetName, string houseNumber) {
-
+void CAddress::updateAddress(string cityName, string streetName, int houseNumber) {
+	this->cityName = cityName;
+	this->streetName = streetName;
+	this->houseNumber = houseNumber;
+}
+CAddress::~CAddress() {
+	
 }
