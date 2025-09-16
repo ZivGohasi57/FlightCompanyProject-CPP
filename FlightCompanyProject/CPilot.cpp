4fcd6294db75fbb33275db3cad0c05bc91308239
Ziv Gohasi <Ziv.Gohasi@s.afeka.ac.il>
2025-09-16 17:25:29 +0300

    commit

--- FlightCompanyProject/CPilot.cpp
+++ FlightCompanyProject/CPilot.cpp
@@ -1,60 +1,113 @@
 #include "CPilot.h"
 #include <cmath>
 #include <iostream>
+
 using namespace std;
 
 CPilot::CPilot(const string& name, bool isCaptain, const CAddress* home)
-    : CCrewMember(name), isCaptain(isCaptain), home(home) {
+    : CCrewMember(name), isCaptain(isCaptain), home(home)
+{
 }
 
 CPilot::CPilot(const CPilot& other)
-    : CCrewMember(other), isCaptain(other.isCaptain), home(other.home) {
+    : CCrewMember(other), isCaptain(other.isCaptain), home(other.home)
+{
 }
 
-CPilot::~CPilot() {}
+CPilot::~CPilot()
+{
+}
 
-bool CPilot::getIsCaptain() const { return isCaptain; }
-const CAddress* CPilot::getHome() const { return home; }
-void CPilot::setIsCaptain(bool captain) { isCaptain = captain; }
-void CPilot::setHome(const CAddress* addr) { home = addr; }
+bool CPilot::getIsCaptain() const
+{
+    return isCaptain;
+}
 
-void CPilot::GetPresent() const {
+const CAddress* CPilot::getHome() const
+{
+    return home;
+}
+
+void CPilot::setIsCaptain(bool captain)
+{
+    isCaptain = captain;
+}
+
+void CPilot::setHome(const CAddress* addr)
+{
+    home = addr;
+}
+
+void CPilot::GetPresent() const
+{
     cout << getMemberName()
         << " thanking the company for receiving the holiday gift" << endl;
 }
-void CPilot::GetUniform() const {
+
+void CPilot::GetUniform() const
+{
     cout << getMemberName()
         << ": this is the fifth time I get a new uniform, this is a waste of money!" << endl;
 }
-void CPilot::GoToSimulator() const {
-    cout << "Pilot " << getMemberName() << " got the message will come soon" << endl;
+
+void CPilot::GoToSimulator() const
+{
+    cout << "Pilot " << getMemberName()
+        << " got the message will come soon" << endl;
 }
 
-bool CPilot::operator+=(int minutes) {
-    if (minutes <= 0) return false;
+bool CPilot::operator+=(int minutes)
+{
+    if (minutes <= 0)
+    {
+        return false;
+    }
     int add = minutes;
-    if (isCaptain) add = static_cast<int>(round(minutes * 1.1));
+    if (isCaptain)
+    {
+        add = static_cast<int>(round(minutes * 1.1));
+    }
     setMemberAirTime(getMemberAirTime() + add);
     return true;
 }
 
-bool CPilot::operator==(const CPilot& other) const {
-    if (getMemberName() != other.getMemberName()) return false;
-    if (home == nullptr && other.home == nullptr) return true;
-    if (home == nullptr || other.home == nullptr) return false;
+bool CPilot::operator==(const CPilot& other) const
+{
+    if (getMemberName() != other.getMemberName())
+    {
+        return false;
+    }
+    if (home == nullptr && other.home == nullptr)
+    {
+        return true;
+    }
+    if (home == nullptr || other.home == nullptr)
+    {
+        return false;
+    }
     return *home == *other.home;
 }
 
-void CPilot::Print(std::ostream& os) const {
+void CPilot::Print(ostream& os) const
+{
     os << "Pilot  " << getMemberName()
         << " minutes " << getMemberAirTime();
-    if (home) os << " Home " << *home;
-    if (isCaptain) os << "  a Captain" << std::endl;
-    else           os << "\n Not a Captain" << std::endl;
+    if (home)
+    {
+        os << " Home " << *home;
+    }
+    if (isCaptain)
+    {
+        os << "  a Captain" << endl;
+    }
+    else
+    {
+        os << "\n Not a Captain" << endl;
+    }
 }
 
-
-ostream& operator<<(ostream& os, const CPilot& pilot) {
+ostream& operator<<(ostream& os, const CPilot& pilot)
+{
     pilot.Print(os);
     return os;
 }
--- FlightCompanyProject/CPlane.h
+++ FlightCompanyProject/CPlane.h
@@ -1,38 +1,39 @@
 #pragma once
 #include <string>
+#include <iostream>
 
 using namespace std;
 
 class CPlane
 {
     friend ostream& operator<<(ostream& os, const CPlane& plane);
+
 private:
     static int s_nextIdentifier;
     int planeIdentifier;
     string planeModel;
     int numOfSeats;
 
-
 public:
     CPlane(const int numOfSeats, const string planeModel);
     CPlane(const CPlane& other);
-    virtual CPlane* Clone() const { return new CPlane(*this); }
+    virtual ~CPlane();
 
+    virtual CPlane* Clone() const
+    {
+        return new CPlane(*this);
+    }
 
-    // getters
     int getPlaneIdentifier() const;
     string getPlaneModel() const;
     int getNumOfSeats() const;
 
-    // setters
     void setPlaneIdentifier(int planeIdentifier);
     void setPlaneModel(string planeModel);
     void setNumOfSeats(int numOfSeats);
 
-    // methods
     bool operator==(const CPlane& other) const;
     void operator=(const CPlane& other);
     CPlane& operator++();
     CPlane operator++(int);
-    virtual ~CPlane();
 };
